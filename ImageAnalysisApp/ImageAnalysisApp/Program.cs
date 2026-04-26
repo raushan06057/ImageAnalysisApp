28fdba7451b0be0b66af7d36d436ad1ccbf5102f
RAUSHAN SINGH <raushanaria184@outlook.com>
2026-04-26 17:24:57 +0530

    Image analysis using ollama(llava) and .net core

---
+++ ImageAnalysisApp/ImageAnalysisApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+Console.WriteLine("Hello, World!");
+try
+{
+    IChatClient chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), "llava");
+
+    string userPrompt="Describe the image";
+    List<ChatMessage> messages = new List<ChatMessage>
+    {
+        new ChatMessage(ChatRole.System, "You are a helpful assistant that describes the image using a direct style."),
+        new ChatMessage(ChatRole.User,userPrompt)
+    };
+
+    var imageFileName = "org.jpg";
+    string image = Path.Combine(Environment.CurrentDirectory,"Images", imageFileName);
+    AIContent aic = new DataContent(File.ReadAllBytes(image),"image/jpeg");
+    var message = new ChatMessage(ChatRole.User, [aic]);
+    messages.Add(message);
+
+    ChatResponse chat = await chatClient.GetResponseAsync(messages);
+    Console.WriteLine(chat.Messages[0].Text);
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"An error occurred: {ex.Message}");
+	throw;
+}
